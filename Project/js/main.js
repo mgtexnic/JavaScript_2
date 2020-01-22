601068ef4c076eb417bc30c10dbbae81e85d53ab
mgtexnic <gmgpi@mail.ru>
2020-01-22 12:34:55 +0300

    The js code is translated to the class structure. Added a button to the html markup as well as styles. Added empty classes for the product basket and individual products. A method for calculating the total cost of all products has been added to the GoodsList class.

--- Project/js/main.js
+++ Project/js/main.js
@@ -1,21 +1,90 @@
-const goods = [
-    { title: "Робот-пылесос xiaomi", price: 20000, img: 'https://via.placeholder.com/150' },
-    { title: "Samsung Galaxy", price: 21500, img: 'https://via.placeholder.com/150' },
-    { title: "Стиральная машина hotpoint", price: 32000, img: 'https://via.placeholder.com/150' },
-    { title: "Умные часы apple watch", price: 26000, img: 'https://via.placeholder.com/150' },
-];
+class GoodsItem {
+    constructor(id, title = 'На данный момент товар отсутствует', price = '-', img = 'images/default_image.jpg') {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.img = img;
+    }
+    render() {
+        return `
+            <div class="goods-item" data-id="${this.id}">
+                <img src="${this.img}" alt="alt">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
+                <button class="js-add-to-cart">Добавить</button>
+            </div>
+        `;
+    }
+}
 
-const renderGoodsItem = (title = "На данный момент товар отсутствует", price = '-', img = 'images/default_images.jpg') => `
-       <div class="goods-item">                                                                                                                    
-            <img src="${img}" alt="alt">
-            <h3>${title}</h3>                                                                                              
-            <p>${price}</p>
-       </div>`;
+class GoodsList {
+    constructor(container) {
+        this.container = document.querySelector(container);
+        this.goods = [];
+    }
+    initListeners() {
+        const buttons = [...this.container.querySelectorAll('.js-add-to-cart')];
+        buttons.forEach(button => {
+            button.addEventListener('click', (event) => {
+                const goodId = event.target.parentElement.getAttribute('data-id');
+                this.addToCart(parseInt(goodId, 10));
+            })
+        })
+    }
+    findGood(id) {
+        return this.goods.find(good => good.id === id);
+    }
+    addToCart(goodId) {
+        const good = this.findGood(goodId);
+        console.log(good);
+    }
+    totalPriceGoods(){
+        let totalPrice = 0;
+        this.goods.forEach(good => {
+            totalPrice += good.price;
+        });
+        return totalPrice;
+    }
+    fetchGoods() {
+        this.goods = [
+            {id: 1, title: "Робот-пылесос xiaomi", price: 20000, img: 'https://via.placeholder.com/250'},
+            {id: 2, title: "Samsung Galaxy", price: 21500, img: 'https://via.placeholder.com/250'},
+            {id: 3, title: "Стиральная машина hotpoint", price: 32000, img: 'https://via.placeholder.com/250'},
+            {id: 4, title: "Умные часы apple watch", price: 26000, img: 'https://via.placeholder.com/250'},
+            {id: 5, title: "Посудомоечная машина bosh", price: 18600, img: 'https://via.placeholder.com/250'},
+        ]
+    }
+    render() {
+        let listHtml = '';
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.id, good.title, good.price, good.img);
+            listHtml += goodItem.render();
+        });
+        this.container.innerHTML = listHtml;
+        this.initListeners();
+    }
+}
 
-const renderGoodsList = (list = [], container) => {
-    const goodsList = list.map(good => renderGoodsItem(good.title, good.price, good.img));
-    document.querySelector(container).innerHTML = goodsList.join('');
-};
+class Basket {
+    countTotalPrice(){
 
-renderGoodsList(goods, '.goods-list');
+    }
 
+    countTotalNumber() {
+
+    }
+
+    outBasketDataToHtml() {
+
+    }
+}
+
+class BasketItem {
+
+
+
+}
+
+const list = new GoodsList('.goods-list');
+list.fetchGoods();
+list.render();
